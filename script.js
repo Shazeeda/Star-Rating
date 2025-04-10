d00ec36ba3adb5fa9fe81fe689e0ed400fb35549
Shazeeda <shazeedamohamed@gmail.com>
2025-04-09 18:26:24 -0700

    added comments to each star

--- script.js
+++ script.js
@@ -2,13 +2,27 @@ const btn = document.querySelector("button");
 const post = document.querySelector(".post");
 const widget = document.querySelector(".star-widget");
 const editBtn = document.querySelector(".edit");
+const header = document.querySelector("header");
 
-btn.onclick = ()=>{
+const feedbackTexts = {
+  "rate-1": "I hate it",
+  "rate-2": "I don't like it",
+  "rate-3": "It's average",
+  "rate-4": "I like it",
+  "rate-5": "I love it"
+};
+
+btn.onclick = () => {
+  const selectedRating = document.querySelector('input[name="rate"]:checked');
+  if (selectedRating) {
+    const ratingId = selectedRating.id;
+    header.textContent = feedbackTexts[ratingId];
     widget.style.display = "none";
     post.style.display = "block";
+  }
+};
 
-    editBtn.onclick = ()=>{
-        widget.style.display = "block";
-        post.style.display = "none";
-    };
+editBtn.onclick = () => {
+  widget.style.display = "block";
+  post.style.display = "none";
 };
